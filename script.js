827d9635466301274dec6fade918bb80adad745b
Nam <realcm471@gmail.com>
2022-08-30 23:10:33 +0700

    Create new user using User.create()

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const mongoose = require('mongoose')
+const User = require("./User")
 
 mongoose.connect("mongodb://localhost/CustomersInfo")
+
+const run = async () => {
+    const user = await User.create({ name: 'Bao Tit', age: 1 })
+    user.name = 'Ngoc Nam'
+    await user.save()
+    // const user = new User({ name: 'Bao Tit', age: 1 })
+    // await user.save()
+    console.log(user);
+}
+
+run()
+
