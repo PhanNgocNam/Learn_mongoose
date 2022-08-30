a6d52c94e2034ef7dc53749aeec7de817859ff8b
Nam <realcm471@gmail.com>
2022-08-30 23:42:55 +0700

    Update UserSchema and fill some field

--- User.js
+++ User.js
@@ -1,8 +1,19 @@
 const { default: mongoose } = require("mongoose");
 
+const addressSchema = new mongoose.Schema({
+    street: String,
+    city: String
+})
+
 const userSchema = new mongoose.Schema({
     name: String,
-    age: Number
+    age: Number,
+    email: String,
+    createAT : Date,
+    updateAt : Date,
+    bestFriend : mongoose.SchemaTypes.ObjectId,
+    hobbies : [String],
+    address : addressSchema
 })
 
 module.exports = mongoose.model('User', userSchema)
--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose')
 const User = require("./User")
+// const addressSchema = require('./User')
 
 mongoose.connect("mongodb://localhost/CustomersInfo")
 
 const run = async () => {
-    const user = await User.create({ name: 'Bao Tit', age: 1 })
-    user.name = 'Ngoc Nam'
-    await user.save()
+    const user = await User.create({
+        name: 'Bao Tit',
+        age: 1,
+        address : {
+            city : 'Bien Hoa City'
+        }
+    })
+
     // const user = new User({ name: 'Bao Tit', age: 1 })
     // await user.save()
     console.log(user);
